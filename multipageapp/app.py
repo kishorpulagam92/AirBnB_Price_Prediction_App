27211b038dcd6a7159d26ff4363135bc185e03f8
Kishor Pulagam <pulagamnkr@gmail.com>
2022-03-01 19:53:11 -0600

    Add files via upload

---
+++ multipageapp/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import home, data, model, Profiling # import your app modules here
+
+app = MultiApp()
+
+st.markdown("""
+# Multi-Page App
+
+This multi-page app is developed by AI-Starter Team.
+""")
+
+# Add all your application here
+app.add_app("Home", home.app)
+app.add_app("Data", data.app)
+app.add_app("Model", model.app)
+app.add_app("Pandas-Profiling", Profiling.app)
+# The main app
+app.run()
